3d8a8b1d3a705959dec5455086a9d711819eecc1
John Mark Harrell <hijohnmark@gmail.com>
2023-11-05 11:20:08 +0700

    completed array filter method lab

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Code your solution here
+
+function findMatching(array, string) {
+    let result = array.filter(item => item.toLowerCase() === string.toLowerCase());
+    return result;
+};
+
+function fuzzyMatch(array, string) {
+    let result = array.filter(item => item.toLowerCase().startsWith(string.toLowerCase()));
+    return result;
+}
+
+function matchName(array, string) {
+    let result = array.filter(item => item.name === string);
+    return result;
+}
